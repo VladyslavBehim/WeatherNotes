bc2c93c420dc338ad08eee70b65c957ad0a8f09a
Vladyslav Behim <vladyslavbehim@gmail.com>
2026-01-12 22:43:26 +0200

    Initial Commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Vladyslav Behim on 12.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
