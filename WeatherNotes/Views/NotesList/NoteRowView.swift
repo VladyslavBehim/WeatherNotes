2044688edcdbefe5922727e1bb2098f7b6e578e1
Vladyslav Behim <vladyslavbehim@gmail.com>
2026-01-14 00:05:02 +0200

    Improve NoteRowView screen and weather badge design

--- WeatherNotes/Views/NotesList/NoteRowView.swift
+++ WeatherNotes/Views/NotesList/NoteRowView.swift
@@ -23,49 +23,59 @@ struct NoteRowView: View {
     }
     
     var body: some View {
-        HStack {
-            VStack(alignment: .leading, spacing: 4) {
+        VStack {
+            HStack{
                 Text(note.title)
                     .font(.headline)
+                    .fontWeight(.semibold)
+                Spacer()
                 Text(formattedDate)
                     .font(.subheadline)
                     .foregroundColor(.gray)
             }
-            
-            Spacer()
-            
-            VStack(spacing: 4) {
-                Text(temperatureText)
+            HStack{
+                Text("Weather conditions")
                     .font(.subheadline)
-                
-                if let url = URL(string: "https://openweathermap.org/img/wn/\(note.icon)@2x.png") {
-                    AsyncImage(url: url) { phase in
-                        switch phase {
-                        case .empty:
-                            ProgressView()
-                                .frame(width: 40, height: 40)
-
-                        case .success(let image):
-                            image
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 40, height: 40)
-
-                        case .failure:
-                            Image(systemName: "network.slash")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 32, height: 32)
-                                .foregroundColor(.red)
-                        
-                        @unknown default:
-                            EmptyView()
+                Spacer()
+                HStack{
+                    Text(temperatureText)
+                        .fontWeight(.semibold)
+                    if let url = URL(string: "https://openweathermap.org/img/wn/\(note.icon)@2x.png") {
+                        AsyncImage(url: url) { phase in
+                            switch phase {
+                            case .empty:
+                                ProgressView()
+                                    .frame(width: 40, height: 40)
+                                
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 40, height: 40)
+                                
+                            case .failure:
+                                Image(systemName: "network.slash")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 32, height: 32)
+                                    .foregroundColor(.red)
+                                
+                            @unknown default:
+                                EmptyView()
+                            }
                         }
                     }
                 }
+                .fontWeight(.semibold)
+                .font(.footnote)
+                .padding(.horizontal)
+                .background(.blue.opacity(0.15))
+                .foregroundColor(.blue)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
             }
+
         }
-        .padding(.vertical, 8)
+        .padding(.vertical, 5)
     }
 }
 
